5cdb06961c89ec1ff8e3196d2c8b5cff05471b8c
Andralearning <andralearning@yahoo.com>
2024-11-23 05:01:01 -0800

    Code video Variables

---
+++ variable.js
@@ -0,0 +1,42 @@
+let name = "Alice"
+console.log(name)
+name = "Edward"
+console.log(name)
+
+// DECLARATION DE VARIABLES
+
+// var
+var age = 15
+console.log(age)
+
+// let
+let email = "alice@alice.fr"
+console.log(email)
+email = "Edward@edward.fr"
+console.log(email)
+
+// const
+const gender = false
+console.log(gender)
+// Créer une erreure : gender = true
+// console.log(gender)
+
+// TYPES DE DONNEES
+
+// String
+const description = "Je suis une chaine de caractère String"
+
+// Number
+const price = 25.85
+
+// Boolean
+const activeProfile = true
+
+// Array
+const toDoList = ["Manger sain", "Boire de l'eau"]
+
+// Object
+const user = {
+    name : "Edward",
+    pass: "Ahefzphfeznfpjez"
+}
